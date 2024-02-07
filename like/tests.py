547328606582bbd6a291e2a9e61d09d9bf6057ad
Patrick <patrick.the.cs.engineer@gmail.com>
2024-02-07 16:47:54 +0200

    Done: Added Unit Tests to Post App Done: Added Unit Tests to Like App

--- like/tests.py
+++ like/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import TestCase, Client
+from .models import Post
+from .models import Like
 
-# Create your tests here.
+
+class LikeTestCase(TestCase):
+    def setUp(self):
+        self.user_1 = User.objects.create_user(username='test_user1', password='Abc123')
+        self.user_2 = User.objects.create_user(username='test_user2', password='Abc123')
+
+        login = self.client.login(username='test_user1', password='Abc123')
+        self.test_post = Post.objects.create(message="Test Message 1", created_by=self.user_1)
+
+    def test_like_with_same_user_1_fail(self):
+        """ Test the New Like cannot work with same user"""
+        client = Client()
+        client.login(username='test_user1', password='Abc123')
+        response = client.post("/posts/like/1/", {"post": self.test_post, "user": self.user_1})
+        self.assertEqual(Like.objects.all().count(), 0)
+
+    def test_like_with_different_user_1_pass(self):
+        """ Test the New Like Succeed with new user"""
+        client = Client()
+        client.login(username='test_user2', password='Abc123')
+        response = client.post("/posts/like/1/", {"post": self.test_post, "user": self.user_2})
+        self.assertEqual(response.status_code, 200)
+
--- post/tests.py
+++ post/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import TestCase, Client
+from .models import Post
 
-# Create your tests here.
+
+class PostTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='test_user', password='Abc123')
+        login = self.client.login(username='test_user', password='Abc123')
+        Post.objects.create(message="Test Message 1", created_by=self.user)
+
+    def test_post_flagged(self):
+        """Test that a post flag is applied"""
+
+        test_post = Post.objects.get(message="Test Message 1")
+        test_post.flagged = True
+        self.assertEqual(test_post.flagged, True)
+
+    def test_details_page_no_login_error(self):
+        """ Test the Post Detail Page no login Failure"""
+        client = Client()
+        response = client.get("/posts/1/")
+        self.assertNotEqual(response.status_code, 200)
+
+    def test_details_page_with_login_pass(self):
+        """ Test the Post Detail Page with login Pass"""
+        client = Client()
+        client.login(username='test_user', password='Abc123')
+        response = client.get("/posts/1/")
+        self.assertEqual(response.status_code, 200)
